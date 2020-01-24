f4b946ed3c5e4b5a2d6d4ccaa23630190aec43b2
Yan Yan <yyan@c1r2p5.hive.fi>
2020-01-24 18:16:25 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Eachshop = (props) =>{
+	const { shop } = props;
+	return (
+		<div className = 'contrainer'>
+			<h3 className='shop_name'>{shop.name} </h3>
+			<img className='image' src = {shop.image} alt = {shop.name}/>
+		</div>
+	)
+}
+
+const Print =(props) =>{
+	const { data } = props;
+	const row = () => data.map(shop =>
+		<Eachshop key= {shop.location} shop = {shop} /> )
+	return(
+		<>
+			{row()}
+		</>
+	)
+}
+const Button = (props) =>{
+	const { sort, value } = props;
+	return (
+		<button className='button' onClick={sort}>
+			  {value}
+		</button>
+	)
+}
+const firstButton = document.getElementsByClassName('button'); 
+
+firstButton.onClick = ()=>{
+	firstButton.style.visibility = "hidden";
+}
+
+const App = ()=> {
+
+	const [ data, setData] = useState([]) 
+
+	const sortShopA = (event) =>{
+	// event.preventDefault();
+	const newData = [...data];
+	newData.sort((a, b) =>{
+		const nameA = a.name.toUpperCase();
+		const nameB = b.name.toUpperCase();
+		if (nameA < nameB) {
+			return -1;
+		  }else if (nameA > nameB) {
+			return 1;
+		  }
+		  return 0;
+	})
+	setData(newData);
+	}
+
+	const sortShopD = (event) =>{
+		// event.preventDefault();
+		const newData = [...data];
+		newData.sort((a, b) =>{
+			const nameA = a.description.toUpperCase();
+			const nameB = b.description.toUpperCase();
+			if (nameA < nameB) {
+				return 1;
+			  }else if (nameA > nameB) {
+				return -1;
+			  }
+			  return 0;
+		})
+		setData(newData);
+		}
+
+	useEffect(()=>{
+		axios
+		  .get('http://localhost:3002/restaurants')
+		  .then(response=>{
+			setData(response.data)
+		  })
+	  }, [])
+
+	  return (
+		  <div>
+			  <Button sort = {sortShopA} value ="Sort restaurants alphabetically ascendingly" />
+			 
+			  <Button sort = {sortShopD} value ="Sort restaurants alphabetically descendingly"/>
+			  {/* <button className='button' onClick={sortShopA}>
+			  Sort restaurants alphabetically
+			  </button> */}
+			  <Print data = {data} />
+		  </div>
+	  )
 }
 
 export default App;
