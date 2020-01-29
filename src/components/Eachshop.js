774bf551eaea18b2926939e67bd7bd1cd734cfba
Yan Yan <yyan@c1r3p8.hive.fi>
2020-01-29 17:31:52 +0200

    changed name

--- src/components/Eachshop.js
+++ src/components/Eachshop.js
@@ -2,18 +2,18 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const EachShop = ({ shop }) =>{
-	const [click, setClick] = useState(0)
+	const [state, setState] = useState(0)
 	const displayTag = ()=>{
-		setClick(1)
+		setState(1)
 	}
 	const hideTag =() =>{
-		setClick(0)
+		setState(0)
 	}
 	let shopTag = `We serve: ${shop.tags} ❤️`
 	return (
 		<div className = 'section' onMouseOver={displayTag} onMouseLeave={hideTag}>
 			<h3 className='shop_name'>{shop.name}</h3>
-			{click===1 &&
+			{state===1 &&
 				<h3 className='shop_tag'>{shopTag}</h3>
 			}
 			<img className='image' src = {shop.image} alt = {shop.name}/>
